3863bb903ad996ab9a9b3d8fb3bf3165c1ceacc6
pavan0209 <pavanasonawane@gmail.com>
2026-06-14 13:00:52 +0530

    initial commit

---
+++ src/main/java/com/pavan/coding/spring_boot_excel_to_db/SpringBootExcelToDbApplication.java
@@ -0,0 +1,12 @@
+package com.pavan.coding.spring_boot_excel_to_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExcelToDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExcelToDbApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pavan/coding/spring_boot_excel_to_db/SpringBootExcelToDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavan.coding.spring_boot_excel_to_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExcelToDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
