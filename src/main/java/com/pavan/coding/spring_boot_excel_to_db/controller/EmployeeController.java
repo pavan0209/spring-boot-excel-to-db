6d7f1031cd2fb435019efe949eab5fe66d198bb4
pavan0209 <pavanasonawane@gmail.com>
2026-06-14 15:00:16 +0530

    Add employee file upload API

---
+++ src/main/java/com/pavan/coding/spring_boot_excel_to_db/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.pavan.coding.spring_boot_excel_to_db.controller;
+
+import com.pavan.coding.spring_boot_excel_to_db.model.Employee;
+import com.pavan.coding.spring_boot_excel_to_db.service.EmployeeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeServiceImpl employeeService;
+
+    @PostMapping(value = "/upload-data", consumes = "multipart/form-data")
+    public ResponseEntity<String> saveFileData(@RequestParam("file") MultipartFile file) throws IOException {
+        employeeService.saveFileData(file.getInputStream());
+        return ResponseEntity.ok("Excel file data saved into Database");
+    }
+}
